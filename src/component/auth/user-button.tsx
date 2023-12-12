c578908d7cc4e06e66fce87756f851bd0ac5a1f9
akbar2habibullah <akbar2habibullah@gmail.com>
2023-12-12 17:04:03 +0700

    update auth component

---
+++ src/component/auth/user-button.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/component/ui/avatar"
+import { Button } from "@/component/ui/button"
+import { auth } from "@/lib/auth"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/component/ui/dropdown-menu"
+import { SignIn, SignOut } from "./auth-component"
+
+export default async function UserButton() {
+	const session = await auth()
+	if (!session?.user) return <SignIn />
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="ghost" className="relative w-8 h-8 rounded-full">
+					<Avatar className="w-8 h-8">
+						{session.user.image && <AvatarImage src={session.user.image} alt={session.user.name ?? ""} />}
+						<AvatarFallback>{session.user.email}</AvatarFallback>
+					</Avatar>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-56" align="end" forceMount>
+				<DropdownMenuLabel className="font-normal">
+					<div className="flex flex-col space-y-1">
+						<p className="text-sm font-medium leading-none">{session.user.name}</p>
+						<p className="text-xs leading-none text-muted-foreground">{session.user.email}</p>
+					</div>
+				</DropdownMenuLabel>
+				<DropdownMenuItem>
+					<SignOut />
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
