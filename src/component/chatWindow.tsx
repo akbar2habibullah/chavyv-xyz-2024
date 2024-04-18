5b4c86cc1de5d0fd95fde59dfaa7e798907f0eb7
akbar2habibullah <akbar2habibullah@gmail.com>
2024-04-18 11:26:20 +0700

    Fix loading spinner

--- src/component/chatWindow.tsx
+++ src/component/chatWindow.tsx
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
 import { Message, useChat } from "ai/react"
-import { useRef, useState, ReactElement, useEffect } from "react"
+import { useRef, useState } from "react"
 import type { FormEvent } from "react"
 
 import { ChatMessageBubble } from "@/component/chatMessageBubble"
@@ -17,17 +17,9 @@ export function ChatWindow(props: { endpoint: string; placeholder?: string; name
 	const messageContainerRef = useRef<HTMLDivElement | null>(null)
 
 	const { endpoint, placeholder } = props
-	const [user, setUser] = useState<string>(`${props.name || "Anonymous User"}`)
+	const [loading, setLoading] = useState<boolean>(false)
 
-	const {
-		messages,
-		input,
-		setInput,
-		handleInputChange,
-		handleSubmit,
-		isLoading: chatEndpointIsLoading,
-		setMessages,
-	} = useChat({
+	const { messages, input, setInput, handleInputChange, setMessages } = useChat({
 		api: endpoint,
 		onError: (e) => {
 			toast(e.message, {
@@ -39,13 +31,13 @@ export function ChatWindow(props: { endpoint: string; placeholder?: string; name
 	async function sendMessage(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault()
 
-		console.log(messages)
 		if (messageContainerRef.current) {
 			messageContainerRef.current.classList.add("grow")
 		}
 		if (!messages.length) {
 			await new Promise((resolve) => setTimeout(resolve, 300))
 		}
+		setLoading(true)
 		const messagesWithUserReply = messages.concat({ id: messages.length.toString(), content: input, role: "user" })
 		setMessages(messagesWithUserReply)
 		setInput("")
@@ -53,7 +45,7 @@ export function ChatWindow(props: { endpoint: string; placeholder?: string; name
 			method: "POST",
 			body: JSON.stringify({
 				messages: messagesWithUserReply,
-				user: props.name || user,
+				user: props.name || "Anonymous User",
 				user_id: props.id,
 			}),
 		})
@@ -61,6 +53,7 @@ export function ChatWindow(props: { endpoint: string; placeholder?: string; name
 		if (response.status === 200) {
 			const newMessages = messagesWithUserReply
 			setMessages(trimMessage([...newMessages, { id: newMessages.length.toString(), content: json.output, role: "assistant" }]))
+			setLoading(false)
 		} else {
 			if (json.error) {
 				toast(json.error, {
@@ -68,6 +61,7 @@ export function ChatWindow(props: { endpoint: string; placeholder?: string; name
 				})
 				throw new Error(json.error)
 			}
+			setLoading(false)
 		}
 	}
 
@@ -92,13 +86,13 @@ export function ChatWindow(props: { endpoint: string; placeholder?: string; name
 				<div className="flex w-full items-center">
 					<textarea className="grow mr-2 p-4 rounded text-black" rows={4} value={input} placeholder={messages.length === 0 ? placeholder : ""} onChange={handleInputChange} />
 					<button type="submit" className="shrink-0 p-2 h-min">
-						<div role="status" className={`${chatEndpointIsLoading ? "" : "hidden"} flex justify-center`}>
+						<div role="status" className={`${loading ? "" : "hidden"} flex justify-center`}>
 							<svg aria-hidden="true" className="w-6 h-6 text-white animate-spin fill-[#FB7C33]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
 								<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
 							</svg>
 						</div>
-						<span className={chatEndpointIsLoading ? "hidden" : ""}>
+						<span className={loading ? "hidden" : ""}>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-send text-[#FB7C33]/50 hover:text-[#FB7C33]">
 								<path d="m22 2-7 20-4-9-9-4Z" />
 								<path d="M22 2 11 13" />
