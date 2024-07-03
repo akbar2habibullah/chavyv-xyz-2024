d35baa77ff70e4ae102dcdadabc323abdb2a93e9
akbar2habibullah <akbar2habibullah@gmail.com>
2024-07-04 03:26:11 +0700

    update

--- src/app/api/chat/retrieval/route.ts
+++ src/app/api/chat/retrieval/route.ts
@@ -146,7 +146,7 @@ Me (My Answer as ${process.env.AGENT}): `
 
 		console.log(completion)
 	
-		const response = trimNewlines(completion.choices[0].text)
+		const response = trimNewlines(completion.choices[0].messages.content)
 
 		const uuid = uid.rnd()
 
