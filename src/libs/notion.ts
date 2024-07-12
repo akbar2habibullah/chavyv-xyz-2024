984daf3936d620bd518b26de36cd6851b353f658
akbar2habibullah <akbar2habibullah@gmail.com>
2024-07-12 16:10:53 +0700

    Update

--- src/libs/notion.ts
+++ src/libs/notion.ts
@@ -16,9 +16,36 @@ export const getPage = async (pageId: string) => {
 	return response
 }
 
+
 export const getBlocks = async (blockId: string) => {
 	const response = await notion.blocks.children.list({
 		block_id: blockId,
-	})
-	return response.results
-}
+	});
+	let blocks = response.results;
+
+	// Recursive function to retrieve children of a block and inject them into the blocks array
+	const getAllChildBlocks = async (blockIndex: number) => {
+		const block = blocks[blockIndex];
+		// @ts-ignore
+		if (block.has_children) {
+			const children = await getBlocks(block.id);
+			// Inject children into the blocks array right after the parent block
+			blocks = [
+				...blocks.slice(0, blockIndex + 1),
+				...children,
+				...blocks.slice(blockIndex + 1),
+			];
+			// Recursively process each child block
+			for (let i = 0; i < children.length; i++) {
+				await getAllChildBlocks(blockIndex + 1 + i);
+			}
+		}
+	};
+
+	// Process each block and retrieve its children if it has any
+	for (let i = 0; i < blocks.length; i++) {
+		await getAllChildBlocks(i);
+	}
+
+	return blocks;
+};
